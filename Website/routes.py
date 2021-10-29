61c7e7982665d7683408351ee4f92d5a13ead8b8
GarrettOHara <garrettohara2018@gmail.com>
2021-10-28 23:33:09 -0700

    updated buttons on create poll page

--- Website/routes.py
+++ Website/routes.py
@@ -11,14 +11,21 @@ def create_poll():
     if request.method == 'POST':
         question = request.form.get('question')
         option = request.form.get('option')
-        print("Question: {} \n Option: {}".format(question,option))
 
-        if len(question) < 10:
-            flash("Your question is too short!", category="error")
-        elif len(option) < 1:
-            flash("Please enter text for option", category="error ")
+        if question == None:
+            flash("Please enter a question", category="error")
+        elif option is None:
+            flash("Please enter a response", category="error")
         else:
-            print("Add data to database")
+            print("Question: {} \nOption: {}".format(question,option))
+
+            if len(question) < 1:
+                flash("Your question is too short!", category="error")
+            elif len(option) < 1:
+                flash("Please enter text for option", category="error")
+            else:
+                flash("Poll Created!", category="success")
+                # Add data to database
 
     return render_template("create_poll.html")
 
