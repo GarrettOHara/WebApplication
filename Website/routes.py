aed855ab423dfe62dfbd3044037ff84bc48e7a02
nick <kokenisnick18@gmail.com>
2021-12-04 13:18:35 -0800

    updated the look of the website and added functionality for an admin user to login and manage polls.  the admin enters a poll id and is able to edit and delete information about polls.

--- Website/routes.py
+++ Website/routes.py
@@ -1,46 +1,41 @@
 # -----------------------------------------------------------
 # Route definitions for Web Application
 #
-# 2021  Garrett O'Hara, Nick Kokenis, Matt Schuiteman
+# 2021  Garrett O'Hara, Nick Kokenis
 # email garrettohara2018@gmail.com
-#       nkokenisXXXX@sdsu.edu
-#       mschuitemanXXX@sdsu.edu
+#       nkokenis3597@sdsu.edu
 # -----------------------------------------------------------
-from os import error
-import urllib
+
+from functools import reduce
+from re import S
 import time
+import urllib
 import socket as sock
 import threading as thread
-from flask.templating import render_template_string
-
-from sqlalchemy.sql.functions import user
 from . import db
+from os import error
 from . import results_graph
 from sqlalchemy import text
+from sqlalchemy.sql.functions import user
 from .models import Answer, Choice, Question
+from flask.templating import render_template_string
 from flask import Blueprint, render_template, request, flash, redirect, url_for, session
 
-
 routes = Blueprint("routes", __name__)
-
 
 @routes.route('/')
 def home():
     sql = text(
-    """
-    SELECT question_id,text FROM question LIMIT 5;
-    """
+        """
+        SELECT question_id,text FROM question LIMIT 5;
+        """
     )
     result = db.engine.execute(sql)
-
     responses = []
     choices = []
-    print("RESULTS:")
     for r in result:
         responses.append(r['question_id'])
         choices.append(r['text'])
-    print(responses)
-    print(choices)
     return render_template("home.html")
 
 @routes.route('/login', methods=['GET', 'POST'])
@@ -48,13 +43,9 @@ def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        print(username)
-        print(password)
-
         if username is None or password is None or len(username) < 1 or len(password) < 1:
-            flash("Username or password entered is too short!", category="error")
+            flash("Error: Please enter a username and password.", category="error")
             return render_template("admin.html")
-  
         sql = text(
             """
             SELECT COUNT(*)
@@ -63,87 +54,101 @@ def login():
             """.format(username,password)
         )
         data = db.engine.execute(sql).fetchall()
-        print(data[0][0])
-        if data[0][0]==0:
-            flash("Error: Invalid login", category="error")
+
+        if data[0][0] == 0:
+            flash("Error: Invalid login.", category="error")
             return render_template("admin.html")
-        sql = text(
-            """
-            SELECT *,COUNT(*) as Repsonses
-            FROM answer, question
-            WHERE answer.question_id = question.question_id
-            GROUP BY answer.question_id 
-            ORDER BY Repsonses DESC;
-            """
-        )
-        data = db.engine.execute(sql).fetchall()
-        print("ADMIN LOGIN SUCCESSFUL")
-        session['admin'] = True
-        return render_template("history.html",data=data)
-
-       
-        
-    elif request.method == 'GET':    
+        else:
+            session['admin'] = True
+            return redirect(url_for('routes.manage_polls'))
+    else:
         return render_template("admin.html")
-    
-    return render_template("admin.html")
 
+@routes.route('/manage_polls', methods=['GET','POST'])
+def manage_polls():
+    if request.method == "POST":
+        poll_id = request.form.get('option')
+        session['pollid'] = poll_id
+        return redirect(url_for('routes.poll_info'))
+    else:
+        return render_template("manage_polls.html")
+
+@routes.route('/poll_info', methods=['GET','POST'])
+def poll_info():
+    poll_id = session.get('pollid', None)
+    if request.method == "GET":
+        sql = text("SELECT * FROM question WHERE question.question_id={}".format(poll_id))
+        result = db.engine.execute(sql).fetchall()
+        return render_template("poll_info.html", data=result)
+    elif request.method == "POST":
+        if request.form.get('delete'):
+            sql = text(
+                """
+                DELETE FROM question WHERE question_id={};
+                """.format(poll_id)
+            )
+            db.engine.execute(sql)
+            sql = text(
+                """
+                DELETE FROM choice WHERE question_id={};
+                """.format(poll_id)
+            )
+            db.engine.execute(sql)
+            sql = text(
+                """
+                DELETE FROM answer WHERE question_id={};
+                """.format(poll_id)
+            )
+            flash("Success: Poll deleted.", category="success")
+        elif request.form.get('edit'):
+            ans = request.form.get('option')
+
+            if ans is None or len(ans) < 1:
+                return redirect(url_for('routes.manage_polls'))
+
+            sql = text(
+                """
+                UPDATE question SET question.text='{}' WHERE question.question_id={};
+                """.format(ans,poll_id)
+            )
+            db.engine.execute(sql)
+            flash("Success: Poll updated.")
+        return redirect(url_for('routes.manage_polls'))
 
 @routes.route('/create_poll', methods=['GET', 'POST'])
 def create_poll():
     if request.method == 'POST':
         question = request.form.get('question')
-        #options = []
-        #while(request.form.get('option') != None):
-        #options.append(request.form.get('option'))
-
         options = request.form.getlist('option')
-
-        print(len(options))
-
-        #print("Question: {} \nOption: {}\nOption: {}".format(
-            #question, option0, option1))
-
         error_control = 0
-
         if len(question) < 1:
-            flash("Your question is too short!", category="error")
+            flash("Error: Please enter a question.", category="error")
             error_control = 1
-        
         for optn in options:
             if len(optn) < 1:
-                flash("Please enter text for option", category="error")
+                flash("Error: Please enter at least two (2) options.", category="error")
                 error_control = 1
 
         if error_control == 0:
-            # CREATE AND WRITE POLL TO DATABASE
             poll = Question(text=question)
             db.session.add(poll)
             db.session.flush()
-
-            # CREATE AND WRITE OPTIONS AND INITIAL
-            # ANSWERS TO THE DATABASE
             op = 'A'
             for option in options:
                 choice = Choice(question_id=poll.question_id,choice=op,text=option)
                 op = chr(ord(op)+1)
                 db.session.add(choice)
                 db.session.flush()
-                print(choice.choice_id)
                 answer = Answer(choice_id=choice.choice_id,question_id=poll.question_id)
                 db.session.add(answer)
-
             session['qid'] = poll.question_id
-
             db.session.commit()
-
-            flash("Poll Created", category="success")
+            flash("Success: Poll created.", category="success")
             return redirect(url_for('routes.share_poll'))
 
     return render_template("create_poll.html")
 
-
-@routes.route('results', methods=['GET', 'POST'])
+@routes.route('/results', methods=['GET', 'POST'])
 def results():
     if request.method == 'GET':
         question = session['qid']
@@ -156,16 +161,13 @@ def results():
             """.format(question)
         )
         result = db.engine.execute(sql)
-
         responses = []
         choices = []
         question = ""
-        print("RESULTS:")
         for r in result:
             responses.append(r['Responses'])
             choices.append(r['Choice'])
-
-        # ORIGINAL GRAPHING METHOD
+        
         th = thread.Thread(target=results_graph.graph_values,args=(choices,responses), daemon=True)
         th.start()
         time.sleep(1)
@@ -174,10 +176,9 @@ def results():
         
     return render_template("results.html")
 
-@routes.route('share_poll', methods=['GET'])
+@routes.route('/share_poll', methods=['GET'])
 def share_poll():
     qid = session['qid']
-    print("SHARE POLL: "+str(qid))
     parameters = {"qid": qid}
     
     s = sock.socket(sock.AF_INET, sock.SOCK_DGRAM)
@@ -186,28 +187,11 @@ def share_poll():
     s.close()
     url = "http://{}:5000/answer_poll?".format(socket)
     share = url+urllib.parse.urlencode(parameters)
-    print(share)
     return render_template("share_poll.html", share=share)
 
 
-@routes.route('/history', methods=['GET', 'POST'])
+@routes.route('/history', methods=['GET'])
 def view_history():
-    if request.method == 'POST':
-        print("HERE")
-        if session['admin']:
-            print(request.form)
-            if request.form == "delete":
-                print(session['admin_poll_data'])
-                flash("Poll Deleted", category="success")
-                return render_template("history.html")
-            if request.form == "edit":
-                print(session['admion_poll_data'])
-                return render_template('edit_poll.html',data=session['admin_poll_data'])
-            print("ERROR")
-            return render_template("history.html",data=session['poll_history'])
-        else:
-            flash("You do not have access to do that", category=error)
-    
     if request.method == 'GET':
         sql = text(
             """
@@ -219,65 +203,34 @@ def view_history():
             """
         )
         data = db.engine.execute(sql).fetchall()
-        session['poll_history'] = data
         return render_template("history.html", data=data)
-
-
-@routes.route('/search', methods=['GET', 'POST'])
-def search():
-    return render_template("search_polls.html")
-
 
 @routes.route('/answer_poll', methods=['GET', 'POST'])
 def answer_poll():
-    # POST REQUEST
     if request.method == "POST":
-        print("POST OF ANSWER")
-
-        # GET ALL ANSWERS
         form = request.form.getlist('keys')
-
-        # USER MUST ANSWER QUESTION, RE-RENDER TEMPLATE
         if len(form) == 0:
-            flash("Please answer the question.", category="error")
+            flash("Error: Please submit an answer.", category="error")
             return render_template("answer_poll.html",question=session['qid'],data=session['question_object'])
-
         else:
-            # CREATE AN ANSWER OBJECT FOR EACH CHOICE THE USER SELECTED
             for key in form:
                 dic = session['question_object'][key]
-                print(dic)
                 choice = dic['choice_id']
                 question = dic['question_id']
                 answer = Answer(choice_id=choice,question_id=question)
                 db.session.add(answer)
-
-            # STORE QUESTION ID FOR RESULTS QUERY
             session['qid'] = question
-
-            # WRITE ANSWERS TO DATABASE
             db.session.commit()
-
-            # DISPLAY SUCCESSFUL WRITE TO USER
-            flash("Response Recorded", category="success")
+            flash("Success: Response recorded.", category="success")
             return redirect(url_for('routes.results'))
-
-    # GET REQUEST
     else:
         qid = request.args.get("qid")
         sql = text("SELECT * FROM question, choice WHERE question.question_id={} AND choice.question_id={}".format(qid,qid))
         result = db.engine.execute(sql)
-
-        # STORE CHOICE ROWS IN LIST AS DICTIONARY OBJECTS
         dic = {}
         for r in result:
             dic[r['choice']] = dict(r.items())
-        print(dic)
-
-        # STORE DICTIONARY OF ROWS IN SESSION
         session['question_object'] = dic
-
-        # GET QUESTION
         sql = text("SELECT text FROM question WHERE question_id={} LIMIT 1".format(qid))
         result = db.engine.execute(sql)
         for r in result:
@@ -285,7 +238,6 @@ def answer_poll():
         session['question'] = question
 
         return render_template("answer_poll.html",question=question,data=dic)
-    
 
 @routes.errorhandler(404)
 def not_found_error(error):
